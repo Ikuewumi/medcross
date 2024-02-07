b23926c73237e5ea2d52de14d31e1c1ef597bd41
Ikuewumi <ayobamsik@gmail.com>
2024-02-07 16:20:44 +0100

    patched pwa functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
       /* your pwa options */
       registerType: 'autoUpdate',
       workbox: {
-        globPatterns: ['**/*.{js,html,css}']
+        globPatterns: ['**\/*.{js,html,css}']
       }
 
     })
