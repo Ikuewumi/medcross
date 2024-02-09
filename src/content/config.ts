e5bf7eae1055180f8af346dfafbfe1ad2aeb5207
Ikuewumi <ayobamsik@gmail.com>
2024-02-09 20:31:33 +0100

    @patch- self-hosted my google font,'Manrope'

--- src/content/config.ts
+++ src/content/config.ts
@@ -16,15 +16,15 @@ const crossword = defineCollection({
     schema: z.object({
         title: z.string(),
         author: reference('author'),
-        image: z.string(),
+        image: z.string().url(),
         description: z.string().optional(),
         categories: z.array(z.string()),
         date: z.date(),
         size: z.number(),
         words: z.array(z.object({
             word: z.string(),
-            start: z.array(z.number()).length(2),
-            end: z.array(z.number()).length(2),
+            start: z.number().array().length(2),
+            end: z.number().array().length(2),
             meaning: z.string()
         }))
     })
